81c761021a83a31f3b76be376ff2e07af536c391
indusy <teririkawaii@outlook.com>
2022-05-17 18:26:59 +0800

    init

---
+++ src/mock/index.ts
@@ -0,0 +1,8 @@
+let funcs = [
+  "Desktop Entry Creator",
+  "Systemd Unit File Creator",
+  "Font config Creator",
+  "Environment Variable Creator",
+  "Wine Configuration Creator",
+  "KDE Window Decoration Creator",
+]
